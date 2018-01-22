fd40d32bffc7e3bb1aaeba28e8b51f5a2acc576c
Michael Koukoullis <mike@kouky.org>
2018-01-22 23:17:17 +1100

    Initial sourcery stencils and marker protocols

---
+++ Sources/EventBus.swift
@@ -0,0 +1,13 @@
+//
+//  EventBus.swift
+//  EventBus-iOS
+//
+//  Created by Michael Koukoullis on 22/1/18.
+//  Copyright © 2018 Michael Koukoullis. All rights reserved.
+//
+
+import Foundation
+
+public protocol EventBusType { }
+
+public protocol EventBusEventType { }
